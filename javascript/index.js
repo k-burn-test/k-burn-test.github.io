8fd9283c89226afabfb646c65c5945f7947ec99e
Kara Burnett <kara.burnett@granicus.com>
2020-03-15 20:01:09 -0500

    test

---
+++ javascript/index.js
@@ -0,0 +1,10 @@
+let select = document.getElementById("region-select");
+let options = ['topic1', 'topic2'];
+
+for(var i = 0; i < options.length; i++) {
+    let opt = options[i];
+    let el = document.createElement("option");
+    el.textContent = opt;
+    el.value = opt;
+    select.add(el);
+}​;
